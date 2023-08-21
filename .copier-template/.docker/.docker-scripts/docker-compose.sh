2d750e37b84519ece66f3cc89c7255d9e98b2144
Young Joon Lee <entelecheia@hotmail.com>
2023-08-22 04:57:32 +0900

    feat(docker-scripts): add login command to docker-compose script
    refactor(docker-scripts): modify command validation logic in docker-compose script

--- .copier-template/.docker/.docker-scripts/docker-compose.sh
+++ .copier-template/.docker/.docker-scripts/docker-compose.sh
@@ -2,17 +2,18 @@
 # add your custom commands here that should be executed when building the docker image
 COMMAND=${1:-"build"}
 VARIANT=${2:-"base"}
-if [ "$COMMAND" != "build" ] && [ "$COMMAND" != "config" ] && [ "$COMMAND" != "push" ]; then
-    echo "Invalid command: $COMMAND"
-    echo "Usage: $0 [build|config] [base|app]"
-    exit 1
-fi
-if [ "$COMMAND" != "build" ]; then
+if [ "$COMMAND" == "build" ]; then
     echo "Building docker image for variant: ${VARIANT}"
-elif [ "$COMMAND" != "push" ]; then
+elif [ "$COMMAND" == "config" ]; then
     echo "Printing docker config for variant: ${VARIANT}"
-else
+elif [ "$COMMAND" == "push" ]; then
     echo "Pushing docker image for variant: ${VARIANT}"
+elif [ "$COMMAND" == "login" ]; then
+    echo "Logging into docker registry for variant: ${VARIANT}"
+else
+    echo "Invalid command: $COMMAND"
+    echo "Usage: $0 [build|config|push|login] [base|app]"
+    exit 1
 fi
 echo "---"
 
@@ -24,6 +25,9 @@ source ".docker/docker.${VARIANT}.env"
 set +a
 if [ "$COMMAND" == "push" ]; then
     docker push "${CONTAINTER_REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}"
+elif [ "$COMMAND" == "login" ]; then
+    echo "GITHUB_CR_PAT: $GITHUB_CR_PAT"
+    docker login ghcr.io -u "$GITHUB_USERNAME"
 else
     docker-compose --project-directory . -f ".docker/docker-compose.${VARIANT}.yaml" "$COMMAND"
 fi

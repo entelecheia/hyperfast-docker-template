f77f4da5eed834de8e7ae92282df74d2d313ffab
Young Joon Lee <entelecheia@hotmail.com>
2023-08-21 14:57:37 +0900

    refactor(docker-script): remove echo for GITHUB_CR_PAT and password-stdin flag in docker login

--- .docker/.docker-scripts/docker-login.sh
+++ .docker/.docker-scripts/docker-login.sh
@@ -3,4 +3,4 @@ echo "Logging into GitHub Container Registry..."
 echo "---"
 echo "GITHUB_CR_PAT: $GITHUB_CR_PAT"
 
-echo "$GITHUB_CR_PAT" | docker login ghcr.io -u "$GITHUB_USERNAME" --password-stdin
+docker login ghcr.io -u "$GITHUB_USERNAME"

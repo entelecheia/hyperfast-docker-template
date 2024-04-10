91bb667073370c32f1cc995ccffadf53d6a46127
Young Joon Lee <entelecheia@hotmail.com>
2024-04-10 13:29:10 +0900

    refactor(entrypoint): improve logging and permissions handling

--- .docker/scripts/entrypoint.sh
+++ .docker/scripts/entrypoint.sh
@@ -3,31 +3,30 @@ VERBOSE=${DOCKER_VERBOSE:-"false"}
 # add your custom commands here that should be executed every time the docker container starts
 if [ "$VERBOSE" = "true" ]; then
     set -x
-fi
-echo "Starting docker container..."
+    echo "Starting docker container..."
 
-# Print out the environment variables
-if [ "$VERBOSE" = "true" ]; then
-    printenv
+    # Print out the environment variables
+    env
+    echo "Fixing permissions..."
 fi
 
 ### Set the USER_UID envvar to match your user.
 # Ensures files created in the container are owned by you:
 #   docker run --rm -it -v /some/path:/invokeai -e USER_UID=$(id -u) <this image>
 # Default UID: 1000 chosen due to popularity on Linux systems. Possibly 501 on MacOS.
-
-echo "Fixing permissions..."
 USER_UID=${USER_UID:-1000}
 USERNAME=${USERNAME:-app}
 LOCAL_UID=$(id -u "$USERNAME")
 WORKSPACE_ROOT=${WORKSPACE_ROOT:-""}
 
 if [ "$USER_UID" != "$LOCAL_UID" ]; then
-    echo "Updating UID and GID to $USER_UID:$USER_UID"
+    echo "Updating UID and GID to $USER_UID:$USER_UID from $LOCAL_UID:$LOCAL_UID"
     usermod -u "$USER_UID" "$USERNAME"
     groupmod -g "$USER_UID" "$USERNAME"
+    echo "Changing ownership of home directory to $USER_UID:$USER_UID"
     chown -R "$USER_UID:$USER_UID" "/home/$USERNAME"
     if [ -n "$WORKSPACE_ROOT" ]; then
+        echo "Changing ownership of workspace directory [$WORKSPACE_ROOT] to $USER_UID:$USER_UID"
         chown -R "$USER_UID:$USER_UID" "$WORKSPACE_ROOT"
     fi
 fi

de2ebe61cf440a7a8dccf21ccf9db54edc80580c
Young Joon Lee <entelecheia@hotmail.com>
2024-04-10 14:27:15 +0900

    chore(config): update Copier commit version to v0.29.9-17-g4c45b97

--- .docker/scripts/entrypoint.sh
+++ .docker/scripts/entrypoint.sh
@@ -15,10 +15,11 @@ fi
 # Ensures files created in the container are owned by you:
 #   docker run --rm -it -v /some/path:/invokeai -e USER_UID=$(id -u) <this image>
 # Default UID: 1000 chosen due to popularity on Linux systems. Possibly 501 on MacOS.
-USER_UID=${USER_UID:-1000}
-USERNAME=${USERNAME:-app}
+USER_UID=${USER_UID:-"1000"}
+USERNAME=${USERNAME:-"app"}
 LOCAL_UID=$(id -u "$USERNAME")
 WORKSPACE_ROOT=${WORKSPACE_ROOT:-""}
+APP_INSTALL_ROOT=${APP_INSTALL_ROOT:-""}
 
 if [ "$USER_UID" != "$LOCAL_UID" ]; then
     echo "Updating UID and GID to $USER_UID:$USER_UID from $LOCAL_UID:$LOCAL_UID"
@@ -26,9 +27,11 @@ if [ "$USER_UID" != "$LOCAL_UID" ]; then
     groupmod -g "$USER_UID" "$USERNAME"
     echo "Changing ownership of home directory to $USER_UID:$USER_UID"
     chown -R "$USER_UID:$USER_UID" "/home/$USERNAME"
-    echo "Changing ownership of opt directory to $USER_UID:$USER_UID"
-    chown -R "$USER_UID:$USER_UID" "/opt"
-    if [ -n "$WORKSPACE_ROOT" ]; then
+    if [ -n "$APP_INSTALL_ROOT" ] && [ -d "$APP_INSTALL_ROOT" ]; then
+        echo "Changing ownership of $APP_INSTALL_ROOT directory to $USER_UID:$USER_UID"
+        chown -R "$USER_UID:$USER_UID" "$APP_INSTALL_ROOT"
+    fi
+    if [ -n "$WORKSPACE_ROOT" ] && [ -d "$WORKSPACE_ROOT" ]; then
         echo "Changing ownership of workspace directory [$WORKSPACE_ROOT] to $USER_UID:$USER_UID"
         chown -R "$USER_UID:$USER_UID" "$WORKSPACE_ROOT"
     fi
